9590b06970d2e3def21edc427136eb5f9b8163c7
Sergeon <mauro.caffarat@gmail.com>
2015-12-21 11:40:45 +0100

    Meto fuel con oil y los ficheros de composer.

---
+++ fuel/app/logs/2015/12/21.php
@@ -0,0 +1,3 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2015-12-21 11:28:58 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
